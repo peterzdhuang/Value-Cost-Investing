c4add99ced8105ed519e9f4a88dae6c7669efe35
peterhuang569 <peterzdhuang@gmail.com>
2023-07-23 15:21:25 -0600

    Update API.py

--- API.py
+++ API.py
@@ -1,1 +1,8 @@
-""" d"""
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+stock = yf.download("vti", interval = "1mo", period = "12mo")
+print(stock)
+list = stock.values.tolist()
+
