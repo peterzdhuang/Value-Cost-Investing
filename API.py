111823c9f5eaac47dccb94cd50074d7a81eac34b
peterhuang569 <peterzdhuang@gmail.com>
2023-07-23 15:25:27 -0600

    Update API.py

--- API.py
+++ API.py
@@ -1,1 +1,11 @@
-""" d"""
+"""=======================================================================
+ * API Class 
+ * Objective: Fetch the Financial data online
+========================================================================"""
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+stock = yf.download("vti", interval = "1mo", period = "12mo")
+print(stock)
+list = stock.values.tolist()
