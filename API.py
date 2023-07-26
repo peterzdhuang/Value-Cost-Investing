738edc0922af28bdb4353f3a5a05016a0ff497c0
peterhuang569 <peterzdhuang@gmail.com>
2023-07-25 22:33:36 -0600

    hi

--- API.py
+++ API.py
@@ -6,6 +6,19 @@ import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-stock = yf.download("vti", interval = "1mo", period = "12mo")
-print(stock)
-list = stock.values.tolist()
+
+class API:
+    def API(self):
+        pass
+    def monthly_returns(self, ticker):
+        """
+        returns the monthly returns in pandas dataframe through the yfinance library
+
+        ticker: symbol that represents a etf; string
+        returns: pandas dataframe
+        """
+        stock = yf.download(ticker, interval = "1mo", period = "1000mo") #putting 1000 months here to get all available data from etf; as like a guaratee
+        stock.reset_index(inplace=True)
+        return stock['Close'] #Only getting the closing price for each month
+
+
--- LumpSum.py
+++ LumpSum.py
@@ -2,3 +2,13 @@
  * API Class 
  * Objective: Calculate the gain for the Lump Sum model
 ========================================================================"""
+
+
+class LumpSum:
+    def LumpSum(self):
+        pass
+    def calculate(self, lump_sum, monthly_returns):
+        stocks_bought = lump_sum / monthly_returns[0]
+
+        return monthly_returns.tail(1) * stocks_bought 
+    
