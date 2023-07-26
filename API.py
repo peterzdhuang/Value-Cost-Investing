4af0ee3e512ba8b604890a514f75a2d8056fcaab
Walter <uno.cool.2013@gmial.com>
2023-07-25 22:39:07 -0600

    oops

--- API.py
+++ API.py
@@ -2,10 +2,23 @@
  * API Class 
  * Objective: Fetch the Financial data online
 ========================================================================"""
-import pandas as pd
 import yfinance as yf
-import matplotlib.pyplot as plt
 
-stock = yf.download("vti", interval = "1mo", period = "12mo")
-print(stock)
-list = stock.values.tolist()
+class API:
+
+    def __init__(self, tkr: str, intvl: str, prd: str):
+        self.tkr=tkr
+        self.intvl=intvl
+        self.prd=prd
+
+        #self.stock = yf.download("vti", interval = "1mo", period = "12mo")
+
+        #retrieve the dataframe of the historical price using the ticker
+        self.stock = yf.download(self.tkr, interval = self.intvl, period = self.prd)
+
+    #return the the closing price as a list of floats
+    def closing_price(self):
+        tmp_dict = self.stock.to_dict()
+        return list(tmp_dict["Close"].value())
+    
+
--- LumpSum.py
+++ LumpSum.py
@@ -2,3 +2,25 @@
  * API Class 
  * Objective: Calculate the gain for the Lump Sum model
 ========================================================================"""
+from API import API
+class LumpSum:
+    def __init__(self, amount: float, tkr: str, intvl: str, prd: str):
+        self.amount=amount
+        self.tkr=tkr
+        self.intvl = intvl
+        self.prd=prd
+
+    #return the profit and annual growth rate of the investment as a list
+    def growth_rate(self):
+        price_list=API(self.tkr, self.intvl, self.prd).closing_price()
+        buying_price=price_list[0]
+        selling_price=price_list[len(price_list)-1]
+        
+        shares=self.amount/buying_price
+        profit=(selling_price-buying_price)*shares
+
+        growth_rate=((selling_price-buying_price)/buying_price)/len(price_list)*12
+
+        return [profit, growth_rate]
+
+
